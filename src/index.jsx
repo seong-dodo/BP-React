3a85e85ea1f14708e177753fb5f165116ea8be67
seong-dodo <dyrnxm52@hanmail.net>
2021-11-11 12:06:14 +0900

    개발환경 구축

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+import App from './App';
+
+import store from './store/store';
+
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  ),
+  document.getElementById('app'),
+);
---
+++ src/store/slice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const { actions, reducer } = createSlice({
+  name: 'application',
+  initialState: {},
+  reducers: { },
+});
+
+export default {
+  actions,
+  reducer,
+};
